c86fe796de6dffa9f05d4b560fbfe5d43cd605e3
Hritik Kumar <hrk10321@gmail.com>
2021-03-01 14:07:48 +0530

    Added Comments

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const express = require("express");
 
 const app = express();
 
+//In middleware we add functions that are hooked into this
+//funnel through which the request goes and we either have
+//next to reach the next middleware or send a response
+// We travel from middleware to middleware from top to bottom
+// using next
+
 app.use((req, res, next) => {
   console.log("In the middleware!");
   next(); //Allows the request to continue to the next middleware in line
