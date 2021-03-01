d45f441dff067284909bbf7639d868e5b2fc9b50
Hritik Kumar <hrk10321@gmail.com>
2021-03-01 19:38:34 +0530

    Modified app.js as per the routing

--- app.js
+++ app.js
@@ -3,33 +3,12 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-//In middleware we add functions that are hooked into this
-//funnel through which the request goes and we either have
-//next to reach the next middleware or send a response
-// We travel from middleware to middleware from top to bottom
-// using next
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-//urlencoded is a like other callback function
-//It uses next at the end
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-//One should see the order in which middlewares will be
-//executed.
-
-//To avoid the order issue we are running this block of code
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  );
-});
-
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<html><h1>Hello ExpressJS</h1></html");
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
