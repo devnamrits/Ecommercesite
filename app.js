2833064bd19a53439c5fbe32c7bd64e5c5a7a63f
Hritik Kumar <hrk10321@gmail.com>
2021-03-01 19:16:23 +0530

    Added simple form and redirection and /product middleware

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
@@ -8,25 +9,27 @@ const app = express();
 // We travel from middleware to middleware from top to bottom
 // using next
 
+//urlencoded is a like other callback function
+//It uses next at the end
+app.use(bodyParser.urlencoded({ extended: true }));
+
 //One should see the order in which middlewares will be
 //executed.
 
 //To avoid the order issue we are running this block of code
-app.use("/", (req, res, next) => {
-  console.log("This always works");
-  next();
-});
-
 app.use("/add-product", (req, res, next) => {
-  console.log("In Add Product Page");
-  res.send("<html><h1>The 'Add Product' page.</h1></html");
-  // ...
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("In another middleware");
   res.send("<html><h1>Hello ExpressJS</h1></html");
-  // ...
 });
 
 app.listen(3000);
