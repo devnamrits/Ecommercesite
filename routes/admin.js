fc9893eedd0ada26e9e34030e461e00b2d50671d
Hritik Kumar <hrk10321@gmail.com>
2021-03-07 08:10:42 +0530

    Indentation

--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,7 @@ const rootDir = require("../util/path");
 
 const router = express.Router();
 
-const products=[];
+const products = [];
 
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
@@ -16,9 +16,9 @@ router.get("/add-product", (req, res, next) => {
 // /admin/add-product => POST
 router.post("/add-product", (req, res, next) => {
   console.log(req.body);
-  products.push({title: req.body.title});
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
-exports.routes=router;
-exports.products=products;
+exports.routes = router;
+exports.products = products;
