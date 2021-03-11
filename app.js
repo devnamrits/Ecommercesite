f5d08425feed708a8ff1000ef6de412930370941
Hritik Kumar <hrk10321@gmail.com>
2021-03-11 18:57:11 +0530

    Added error Controllers

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.set('view engine', 'ejs');
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const errorController = require("./controllers/error404")
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -16,11 +17,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminRoutes.routes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('error404', {
-    pageTitle: 'Error 404'
-  });
-  //res.status(404).sendFile(path.join(__dirname, "views", "error404.html"));
-});
+app.use(errorController.get404);
 
 app.listen(3000);
---
+++ controllers/error404.js
@@ -0,0 +1,7 @@
+exports.get404 = (req, res, next) => {
+    res.status(404).render('error404', {
+        pageTitle: 'Error 404',
+        path: ''
+    });
+    //res.status(404).sendFile(path.join(__dirname, "views", "error404.html"));
+}
