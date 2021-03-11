185482f37d79d3c5112e51184165b1d0ac54a99a
Hritik Kumar <hrk10321@gmail.com>
2021-03-11 09:43:50 +0530

    Added object to passed on

--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,14 @@ const router = express.Router();
 
 router.get("/", (req, res, next) => {
   console.log("shop.js", adminData.products);
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  //res.sendFile(path.join(rootDir, "views", "shop.html"));
+  res.render("shop", {
+    pageTitle: "Shop",
+    prods: adminData.products,
+    path: "/",
+    activeShop: true,
+    productCSS: true,
+  });
 });
 
 module.exports = router;
